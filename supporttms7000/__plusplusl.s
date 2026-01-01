0d02876c1e5fbfb8356c46abdcfb0987233c1a17
Alan Cox <etchedpixels@gmail.com>
2026-01-01 19:38:10 +0000

    tms7000: comment plusplusl helper

--- supporttms7000/__plusplusl.s
+++ supporttms7000/__plusplusl.s
@@ -10,28 +10,28 @@ __plusplusl:
 	mov a,b
 	add r13,a
 	push st
-	sta *r5
+	sta *r5		; low byte old into b, result into *r5
 	decd r5
-	lda *r5
-	mov a,r13
+	lda *r5		; second byte
+	mov a,r13	; load into r13 (free now)
 	pop st
-	adc r12,a
+	adc r12,a	; into a
 	push st
-	sta *r5
+	sta *r5		; into *r5
 	decd r5
-	lda *r5
+	lda *r5		; same again but can put the value into the right spot
 	mov a,r3
 	pop st
 	adc r11,a
 	push st
 	sta *r5
 	decd r5
-	lda *r5
+	lda *r5		; last byte into r2
 	mov a,r2
 	pop st
 	adc r10,a
 	sta *r5
-	mov b,r5
+	mov b,r5	; shuffle the other 2 bytes
 	mov r13,r4
 	rets
 
