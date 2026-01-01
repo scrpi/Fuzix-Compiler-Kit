37675030ee81ca0f2f8346c7d06da438905256ee
Alan Cox <etchedpixels@gmail.com>
2026-01-01 19:38:01 +0000

    tms7000: fix plusplus helper

--- supporttms7000/__plusplus.s
+++ supporttms7000/__plusplus.s
@@ -6,30 +6,28 @@
 ;	return original data
 ;
 __plusplus:
-	lda *r4
-	mov a,r3
+	lda *r5
+	mov a,r2
 	add %1,r5
 	adc %0,r4
-	lda *r4
-	mov a,r2
+	lda *r5
+	mov a,r3
 	; R2/R3 is the original
-	add r2,r12
-	adc r3,r13
-	mov r13,a
-	sta *r13
-	decd r4
-	mov r12,a
-	sta *r13
+	add r3,r11
+	adc r2,r10
+	mov r11,a
+	sta *r5
+	decd r5
+	mov r10,a
+	sta *r5
 	movd r3,r5
 	rets
 
 __plusplusc:
-	lda *r4
+	lda *r5
 	mov a,b
-	add r13,a
-	sta *r4
+	add r10,a
+	sta *r5
 	mov b,r5
 	clr r4
 	rets
-
-	
