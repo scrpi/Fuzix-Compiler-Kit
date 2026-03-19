b25c78979c79ba54882e1623cded303a41883e8b
Alan Cox <etchedpixels@gmail.com>
2026-03-19 15:27:52 +0000

    wtests: test 133 is C99 and we are C89

--- test/wtests/input133.c
+++ test/wtests/input133.c
@@ -6,4 +6,5 @@ int fred[23];
 char mary[100];
 extern char mary[];
 
-void main() { cprintf("OK\n"); }
+/* For C99 testing this is void main() and meaningful but we are C89 for now */
+int main() { cprintf("OK\n"); return 0; }
