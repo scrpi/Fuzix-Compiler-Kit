9a3ab9f42008dee7f0f5065c94d5a39d6bb67ec8
Alan Cox <etchedpixels@gmail.com>
2026-03-19 13:40:52 +0000

    input022; wtest has ancient debug in it - kill

--- test/wtests/input022.c
+++ test/wtests/input022.c
@@ -7,7 +7,6 @@ int main() {
 
   sum= 10; sum= sum * 10;          cprintf("%d\n", sum);
 
-  // This one doesn't work as at commit 1ba062c
   sum= 10; sum *= 10;              cprintf("%d\n", sum);
 
   sum = sum + (*str - '0'); str++; cprintf("%d\n", sum);
