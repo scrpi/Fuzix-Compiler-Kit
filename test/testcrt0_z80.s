964ca73f69d2875523bffd59a69659225c945423
Alan Cox <etchedpixels@gmail.com>
2026-03-19 13:47:21 +0000

    z80: enable wtest

--- test/testcrt0_z80.s
+++ test/testcrt0_z80.s
@@ -4,3 +4,25 @@
 	call	_main
 	ld	a,l
 	out	(0xFF),a
+
+	.export	_printint
+
+_printint:
+	pop	hl
+	pop	de
+	push	de
+	ld	a,e
+	out	(0xFC),a
+	ld	a,d
+	out	(0xFD),a
+	jp	(hl)
+
+	.export	_printchar
+
+_printchar:
+	pop	hl
+	pop	de
+	push	de
+	ld	a,e
+	out	(0xFE),a
+	jp	(hl)
