51a0c9c4858b7e3be74703805f39d4ee15dd1599
Alan Cox <etchedpixels@gmail.com>
2026-04-05 20:50:45 +0100

    6502: more work on -Os sizing
    Compact deref of a local as it's common. Needs to be moved into the
    compiler later

--- support6502/__gloy.s
+++ support6502/__gloy.s
@@ -7,6 +7,10 @@
 	.export __pushly0
 	.export __gloytmp
 	.export __gloytmp0
+	.export __gloytmpd1
+	.export __gloytmpd2
+	.export __pushyd1
+	.export __pushyd2
 
 	.code
 __gloy0:
@@ -31,3 +35,26 @@ __gloytmp:
 	sta @tmp
 	stx @tmp+1
 	rts
+
+__gloytmpd1:
+	jsr __gloytmp
+	ldx #0
+	lda (@tmp,x)
+	rts
+
+__gloytmpd2:
+	jsr __gloytmp
+	ldy #1
+	lda (@tmp),y
+	tax
+	dey
+	lda (@tmp),y
+	rts
+
+__pushyd1:
+	jsr __gloytmpd1
+	jmp __push
+
+__pushyd2:
+	jsr __gloytmpd2
+	jmp __push
