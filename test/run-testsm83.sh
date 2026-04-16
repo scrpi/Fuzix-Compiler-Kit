ad24391764e01416863af8ef460af014ae9a81c3
Alan Cox <etchedpixels@gmail.com>
2026-04-16 22:13:21 +0100

    sm83/gb: run test

---
+++ test/run-testsm83.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+for i in tests/*.c
+do
+	b=$(basename $i .c)
+	echo  $b":"
+	fcc -O -mgb -c tests/$b.c
+	ldgb -b -C0 testcrt0_sm83.o tests/$b.o -o tests/$b /opt/fcc/lib/sm83/libsm83.a -m tests/$b.map
+	./emusm83 tests/$b tests/$b.map
+	rm -f tests/$b tests/$b.o tests/$b.map
+done
