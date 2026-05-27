175e05dcad54067d947cfd0427a30adf0e2d0355
Alan Cox <etchedpixels@gmail.com>
2026-05-27 11:54:38 +0100

    0511: add more torture

--- test/tests/0511-sub.c
+++ test/tests/0511-sub.c
@@ -4,10 +4,25 @@ int sub(char *p) {
         return v;
 }
 
+char subc(void) {
+        char d = 0x00, m = 0xf0, x = 0xff;
+        return m & d | ~m & x;
+}
+
+char sub3(void) {
+        char x = 1, y = 2;
+        char tmp = 3 - (y << 1);
+        return x;
+}
+
 int main(int argc, char *argv[])
 {
         char x;
         if (sub(&x) != 100)
                 return 1;
+        if (subc() != 0x0F)
+                return 2;
+        if (sub3() != 1)
+                return 3;
         return 0;
 }
