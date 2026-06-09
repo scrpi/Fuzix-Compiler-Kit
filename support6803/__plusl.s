926a2f3cb743ba4eeef67ac27c6cbb3d11caf6e5
Alan Cox <etchedpixels@gmail.com>
2026-06-09 16:22:53 +0100

    6303/6803: add __plusl helper

---
+++ support6803/__plusl.s
@@ -0,0 +1,21 @@
+;
+;	Add (TOS) to hireg:D and remove from stack
+;
+	.export __plusl
+	.setcpu 6803
+
+__plusl:
+	tsx
+	addd	4,x		; low halves
+	std	@tmp
+	ldd	@hireg
+	adcb	3,x
+	adca	2,x
+	sta	@hireg
+	lda	@tmp
+	pulx
+	ins
+	ins
+	ins
+	ins
+	jmp	,x
